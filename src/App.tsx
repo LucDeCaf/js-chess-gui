ed458314ad93ca3dc35495cd1d5510b9c1d55345
LucDeCaf <ldecafmeyer@gmail.com>
2024-05-14 13:46:31 +0200

    feat: display pieces, improve types, refactor piece list (to be done further)

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,57 @@
 import { VariantProps, cva } from "class-variance-authority";
 import { For, createSignal } from "solid-js";
+import whitePawnImage from "./assets/pieces/wP.svg";
+import whiteKnightImage from "./assets/pieces/wP.svg";
+import whiteBishopImage from "./assets/pieces/wP.svg";
+import whiteRookImage from "./assets/pieces/wP.svg";
+import whiteQueenImage from "./assets/pieces/wP.svg";
+import whiteKingImage from "./assets/pieces/wP.svg";
+import blackPawnImage from "./assets/pieces/wP.svg";
+import blackKnightImage from "./assets/pieces/wP.svg";
+import blackBishopImage from "./assets/pieces/wP.svg";
+import blackRookImage from "./assets/pieces/wP.svg";
+import blackQueenImage from "./assets/pieces/wP.svg";
+import blackKingImage from "./assets/pieces/wP.svg";
 
-enum Piece {
-    NONE,
-    WHITE_PAWN,
-    WHITE_KNIGHT,
-    WHITE_BISHOP,
-    WHITE_ROOK,
-    WHITE_QUEEN,
-    WHITE_KING,
-    BLACK_PAWN,
-    BLACK_KNIGHT,
-    BLACK_BISHOP,
-    BLACK_ROOK,
-    BLACK_QUEEN,
-    BLACK_KING,
+enum Kind {
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING,
 }
 
-function createBoard(): Piece[] {
+enum Color {
+    WHITE,
+    BLACK,
+}
+
+type Piece = {
+    kind: Kind;
+    color: Color;
+};
+
+function initialisePieces(): (Piece | null)[] {
     const board = [];
     for (let i = 0; i < 64; i++) {
-        board.push(Piece.NONE);
+        board.push({ kind: Kind.PAWN, color: Color.WHITE });
     }
     return board;
 }
 
 const App = () => {
-    const [board, _setBoard] = createSignal<Piece[]>(createBoard());
+    const [pieces, _setPieces] = createSignal<(Piece | null)[]>(
+        initialisePieces()
+    );
     // const [selected, setSelected] = createSignal<number | null>(null);
 
     return (
-        <div class="w-full h-screen flex justify-center items-center">
+        <div class="w-full p-12 flex justify-center">
+            {/* Board */}
             <div class="grid grid-cols-8 gap-0 w-max h-max rounded-lg overflow-hidden">
-                <For each={board()}>
-                    {(_board, i) => {
+                <For each={Array.from({ length: 64 }, () => 0)}>
+                    {(_, i) => {
                         let offset = Math.floor(i() / 8) % 2 == 0 ? 0 : 1;
                         return (
                             <Square
@@ -45,6 +63,14 @@ const App = () => {
                     }}
                 </For>
             </div>
+            {/* Pieces */}
+            <For each={pieces()}>
+                {(piece, _i) => {
+                    if (piece) {
+                        return <VisualPiece piece={piece} />;
+                    }
+                }}
+            </For>
         </div>
     );
 };
@@ -63,5 +89,62 @@ interface SquareProps extends VariantProps<typeof square> {}
 const Square = (props: SquareProps) => {
     return <div class={square({ color: props.color })}></div>;
 };
+
+interface PieceProps {
+    piece: Piece;
+}
+
+const VisualPiece = (props: PieceProps) => {
+    const imagePath = getPieceImage(props.piece);
+
+    if (!imagePath) {
+        throw new Error(`failed to find image for piece ${props.piece}`);
+    }
+
+    return (
+        <div class="w-40 aspect-square absolute">
+            <img src={imagePath} alt="piece" />
+        </div>
+    );
+};
+
+function getPieceImage(piece: Piece): string | null {
+    switch (piece.kind) {
+        case Kind.PAWN:
+            return piece.color === Color.WHITE
+                ? whitePawnImage
+                : blackPawnImage;
+            break;
+        case Kind.KNIGHT:
+            return piece.color === Color.WHITE
+                ? whiteKnightImage
+                : blackKnightImage;
+            break;
+        case Kind.BISHOP:
+            return piece.color === Color.WHITE
+                ? whiteBishopImage
+                : blackBishopImage;
+            break;
+        case Kind.ROOK:
+            return piece.color === Color.WHITE
+                ? whiteRookImage
+                : blackBishopImage;
+            break;
+        case Kind.QUEEN:
+            return piece.color === Color.WHITE
+                ? whiteQueenImage
+                : blackBishopImage;
+            break;
+        case Kind.KING:
+            return piece.color === Color.WHITE
+                ? whiteKingImage
+                : blackBishopImage;
+            break;
+
+        default:
+            return null;
+            break;
+    }
+}
 
 export default App;
